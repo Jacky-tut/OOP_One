ba444cfc7e1921007fe04e9113c5242d67c06bb5
Jacky_tut <netfinnov@gmail.com>
2023-10-05 15:25:02 +0300

    fin

---
+++ lesson1/src/main/java/ru/geekbrains/lesson1/VendingMachine.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.lesson1;
+
+import java.util.List;
+
+public class VendingMachine {
+
+
+    private List<Product> products;
+
+    public VendingMachine(List<Product> products) {
+        this.products = products;
+    }
+
+
+    public BottleOfMilk getBottleofMilk(double volume, int fat){
+        for (Product product : products){
+            if (product instanceof BottleOfMilk){
+                BottleOfMilk bottleOfMilk = (BottleOfMilk) product;
+                if (bottleOfMilk.getVolume() == volume && bottleOfMilk.getFat() == fat){
+                    return bottleOfMilk;
+                }
+            }
+        }
+        return null;
+    }
+    public Chocolate getChocolate(double calories){
+        for (Product product : products){
+            if (product instanceof Chocolate){
+                Chocolate chocolate = (Chocolate) product;
+                if (chocolate.getCalories() == calories){
+                    return chocolate;
+                }
+            }
+        }
+        return null;
+    }
+}
