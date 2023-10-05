d4a255856d4ba3ebd4392b701a1c24ed055f7111
Jacky_tut <netfinnov@gmail.com>
2023-10-05 15:30:13 +0300

    final

--- lesson1/src/main/java/ru/geekbrains/lesson1/Chocolate.java
+++ lesson1/src/main/java/ru/geekbrains/lesson1/Chocolate.java
@@ -1,2 +1,23 @@
-package ru.geekbrains.lesson1;public class Chocolate {
+package ru.geekbrains.lesson1;
+
+public class Chocolate extends Product{
+    private double calories;
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double volume) {
+        this.calories = calories;
+    }
+
+    public Chocolate(String brand, String name, double price, double calories){
+        super(brand, name, price);
+        this.calories = calories;
+    }
+
+    @Override
+    public String displayInfo() {
+        return String.format("ШОКОЛАД %s - %s - %.2f - калории: %.2f", brand, name, price, calories);
+    }
 }
